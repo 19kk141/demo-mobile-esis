47dfd6c6fa388086d8b6f7a594944639f17e1108
19kk141 <19kk141@esisalama.org>
2022-07-05 17:44:38 +0200

    tp fini

--- app/src/main/java/org/esissalama/mobile/demo/MainActivity.java
+++ app/src/main/java/org/esissalama/mobile/demo/MainActivity.java
@@ -3,12 +3,31 @@ package org.esissalama.mobile.demo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    int[] img = {
+            R.drawable.dice_1,R.drawable.dice_2,R.drawable.dice_3,
+            R.drawable.dice_4,R.drawable.dice_5,R.drawable.dice_6
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView image = findViewById(R.id.img1);
+        Button button = findViewById(R.id.btn1);
+
+       button.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               image.setImageResource(img[new Random().nextInt(6)]);
+           }
+       });
     }
 }
